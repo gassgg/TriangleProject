4aed8dfdd1e7c1c599f38dee4cf00d54528dada3
Mikhail <mikhail.lisin.312@mail.ru>
2023-02-03 15:20:07 +0300

    Добавление модели фотоаппарата

--- TriangleProject/main.cpp
+++ TriangleProject/main.cpp
@@ -32,8 +32,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+const std::string MODEL_PATH = "models/1234_aparat_stereo_glass.obj";
+const std::string TEXTURE_PATH = "textures/1234_aparat_stereo_glass.jpg";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
